fd8dcc83146c71332a2d6d5f40a196a9f9b4cf35
Stefano Fancello <stefano.fancello@nethesis.it>
2024-01-04 16:07:44 +0100

    remove chroma persistence

--- src/rag_lcel.py
+++ src/rag_lcel.py
@@ -1,35 +1,28 @@
-import os
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-db_path = "./chroma_db"
+# Document loader
+# https://python.langchain.com/docs/modules/data_connection/document_loaders/
+# https://python.langchain.com/docs/integrations/document_loaders/
+from langchain_community.document_loaders import YoutubeLoader
+loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=8fEEbKJoNbU")
+documents = loader.load()
 
-if os.path.exists(db_path):
-    # if db already exists, we can load it
-    db = Chroma(persist_directory=db_path, embedding_function=OpenAIEmbeddings())
-else:
-    # Document loader
-    # https://python.langchain.com/docs/modules/data_connection/document_loaders/
-    # https://python.langchain.com/docs/integrations/document_loaders/
-    from langchain_community.document_loaders import YoutubeLoader
-    loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=8fEEbKJoNbU")
-    documents = loader.load()
+# Split documents with text splitter
+# https://python.langchain.com/docs/modules/data_connection/document_transformers/
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=0,
+    length_function=len,
+)
+chunks = []
+for document in documents:
+    chunks += (text_splitter.create_documents([document.page_content], [document.metadata]))
 
-    # Split documents with text splitter
-    # https://python.langchain.com/docs/modules/data_connection/document_transformers/
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=0,
-        length_function=len,
-    )
-    chunks = []
-    for document in documents:
-        chunks += (text_splitter.create_documents([document.page_content], [document.metadata]))
-
-    # Store our documents in a vector store https://python.langchain.com/docs/modules/data_connection/vectorstores/
-    # db is persistent and can be reused
-    db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory=db_path)
+# Store our documents in a vector store https://python.langchain.com/docs/modules/data_connection/vectorstores/
+# db is persistent and can be reused
+db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory="./chroma_db")
 
 # Chat model with stdout streaming output
 from langchain.chat_models import ChatOpenAI
@@ -42,14 +35,6 @@ from langchain.retrievers.multi_query import MultiQueryRetriever
 retriever = MultiQueryRetriever.from_llm(
     retriever=db.as_retriever(), llm=llm
 )
-
-questions = [
-    "What is effective accelerationism?",
-    "What is Kardashev scale?",
-    "What energy sources could provide the needed energy?",
-    "What is the difference between e/acc and effective altruism?",
-    "What can you say abou black holes?",
-]
 
 # Get the template from langchain hub https://smith.langchain.com/hub
 from langchain import hub
@@ -64,6 +49,15 @@ rag_chain = (
     | llm
     | StrOutputParser()
 )
+
+# questions to ask
+questions = [
+    "What is effective accelerationism?",
+    "What is Kardashev scale?",
+    "What energy sources could provide the needed energy?",
+    "What is the difference between e/acc and effective altruism?",
+    "What can you say abou black holes?",
+]
 
 # invoke the LCEL chain for each question
 for question in questions:
--- src/rag_verbose.py
+++ src/rag_verbose.py
@@ -1,35 +1,28 @@
-import os
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 
-db_path = "./chroma_db"
+# Document loader
+# https://python.langchain.com/docs/modules/data_connection/document_loaders/
+# https://python.langchain.com/docs/integrations/document_loaders/
+from langchain_community.document_loaders import YoutubeLoader
+loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=8fEEbKJoNbU")
+documents = loader.load()
 
-if os.path.exists(db_path):
-    # if db already exists, we can load it
-    db = Chroma(persist_directory=db_path, embedding_function=OpenAIEmbeddings())
-else:
-    # Document loader
-    # https://python.langchain.com/docs/modules/data_connection/document_loaders/
-    # https://python.langchain.com/docs/integrations/document_loaders/
-    from langchain_community.document_loaders import YoutubeLoader
-    loader = YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=8fEEbKJoNbU")
-    documents = loader.load()
+# Split documents with text splitter
+# https://python.langchain.com/docs/modules/data_connection/document_transformers/
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=0,
+    length_function=len,
+)
+chunks = []
+for document in documents:
+    chunks += (text_splitter.create_documents([document.page_content], [document.metadata]))
 
-    # Split documents with text splitter
-    # https://python.langchain.com/docs/modules/data_connection/document_transformers/
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=2000,
-        chunk_overlap=0,
-        length_function=len,
-    )
-    chunks = []
-    for document in documents:
-        chunks += (text_splitter.create_documents([document.page_content], [document.metadata]))
-
-    # Store our documents in a vector store https://python.langchain.com/docs/modules/data_connection/vectorstores/
-    # db is persistent and can be reused
-    db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory=db_path)
+# Store our documents in a vector store https://python.langchain.com/docs/modules/data_connection/vectorstores/
+# (optional) add persist_directory so we can reuse the db without re-creating it
+db = Chroma.from_documents(chunks, OpenAIEmbeddings(), persist_directory="./chroma_db")
 
 # Chat model with stdout streaming output
 from langchain.chat_models import ChatOpenAI
@@ -39,14 +32,6 @@ llm = ChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], t
 # Create a retriever with our vector store
 # (optional) return k most similar documents
 retriever = db.as_retriever(search_kwargs={"k": 3})
-
-questions = [
-    "What is effective accelerationism?",
-    "What is Kardashev scale?",
-    "What energy sources could provide the needed energy?",
-    "What is the difference between e/acc and effective altruism?",
-    "What can you say abou black holes?",
-]
 
 # Create a prompt template
 from langchain.prompts import ChatPromptTemplate, PromptTemplate, HumanMessagePromptTemplate
@@ -61,6 +46,15 @@ prompt = ChatPromptTemplate(
             )
     ]
 )
+
+# list of questions to ask
+questions = [
+    "What is effective accelerationism?",
+    "What is Kardashev scale?",
+    "What energy sources could provide the needed energy?",
+    "What is the difference between e/acc and effective altruism?",
+    "What can you say abou black holes?",
+]
 
 # put all together
 for question in questions:
