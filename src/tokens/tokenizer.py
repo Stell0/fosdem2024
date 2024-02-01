d28fff630c4c588ff84a4dcefcb1df37d28010a4
Stefano Fancello <stefano.fancello@nethesis.it>
2024-02-01 11:37:57 +0100

    Removed tokens

--- src/tokens/tokenizer.py
+++
@@ -1,5 +0,0 @@
-from transformers import GPT2Tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-tokens = tokenizer.tokenize("Hello, This is an example of tokenization!")
-print("Tokens:", tokens)
-print("Tokens length:", len(tokens))
